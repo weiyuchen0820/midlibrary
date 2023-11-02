133cdc78009331557128b09ee755b8e886439add
weiyuchen0820 <10956036@ntub.edu.tw>
2023-11-02 20:25:23 +0800

    123

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from mysite.models import Post
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','slug','pub_date')
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
